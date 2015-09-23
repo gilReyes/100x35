8e9927b80224dc88c2475e948f9c88e7c5d7d9f4
100x35DB <100x35DB@gmail.com>
2015-09-23 19:52:47 -0400

    Inbox Done

---
+++ 100x35VS/Web/Scripts/inbox.js
@@ -0,0 +1,80 @@
+﻿var Inbox = function () {
+    var handleEditorInit = function () {
+        $('.inbox-wysihtml5').wysihtml5({
+            "stylesheets": ["../../assets/global/plugins/bootstrap-wysihtml5/wysiwyg-color.css"]
+        });
+    }
+
+    var handleCompose = function () {
+        $('#ComposeBtn').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: siteUrl + "Inbox/Compose",
+                type: "GET"
+            }).success(function (data) {
+                $('#InboxBody').html(data);
+                handleEditorInit();
+                handleCompose();
+                handleInbox();
+            }).error(function () {
+                showError();
+            });
+        });
+    }
+
+    var handleReply = function () {
+        $('#ReplyBtn').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: siteUrl + "Inbox/Reply",
+                type: "GET"
+            }).success(function (data) {
+                $('#InboxBody').html(data);
+                handleCompose();
+                handleInbox();
+            }).error(function () {
+                showError();
+            });
+        });
+    }
+
+    var handleInbox = function() {
+        $('#InboxBtn').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: siteUrl + "Inbox/Inbox",
+                type: "GET"
+            }).success(function (data) {
+                $('#InboxBody').html(data);
+                handleRead();
+                handleCompose();
+            }).error(function () {
+                showError();
+            });
+        });
+    }
+
+    var handleRead = function () {
+        $('.message').on('click', function (e) {
+            e.preventDefault();
+            $.ajax({
+                url: siteUrl + "Inbox/View",
+                type: "GET",
+                data: { id: $(this).attr('data-messageid') }
+            }).success(function (data) {
+                $('#InboxBody').html(data);
+                handleCompose();
+                handleReply();
+            }).error(function () {
+                showError();
+            });
+        })
+    }
+
+    return {
+        init: function () {
+            handleInbox();
+            handleRead();
+        }
+    }
+}();

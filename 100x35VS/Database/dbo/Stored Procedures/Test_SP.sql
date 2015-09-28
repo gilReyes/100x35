fee89d5639242dc12af9550f8301737a024a2936
Stephan Elias Remy <stephan.elias@upr.edu>
2015-09-28 16:43:20 -0400

    Added DB to solution

---
+++ 100x35VS/Database/dbo/Stored Procedures/Test_SP.sql
@@ -0,0 +1,14 @@
+﻿
+-- =============================================
+-- Author:		<Stephan Elias>
+-- Create date: <Create Date,,>
+-- Description:	<Test Procedure>
+-- =============================================
+CREATE PROCEDURE Test_SP 
+	@ID int
+AS
+BEGIN
+	SELECT *
+	FROM Test T
+	WHERE @ID = T.ID
+END

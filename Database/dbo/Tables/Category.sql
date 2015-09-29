9f04438a96975d6f9557e11e642f257c8e812979
Stephan Elias Remy <stephan.elias@upr.edu>
2015-09-29 02:35:18 -0400

    Changes to DB

--- Database/dbo/Tables/Category.sql
+++ Database/dbo/Tables/Category.sql
@@ -2,6 +2,6 @@
 (
 	[Category_Id] INT NOT NULL PRIMARY KEY, 
     [Name] VARCHAR(25) NOT NULL, 
-    [Category_Type_Id] NCHAR(10) NOT NULL, 
+    [Category_Type_Id] INT NOT NULL, 
     CONSTRAINT [FK_Category_Category_Type] FOREIGN KEY ([Category_Type_Id]) REFERENCES [Category_Type]([Category_Type_Id])
 )
